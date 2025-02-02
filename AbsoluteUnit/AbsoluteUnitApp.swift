97871a704b06eea9ba81ddf2a0af27ad47da55f3
GreenRamSilgen <kiranshrs@yahoo.com>
2025-02-01 16:10:57 -0800

    Initial Commit

---
+++ AbsoluteUnit/AbsoluteUnitApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbsoluteUnitApp.swift
+//  AbsoluteUnit
+//
+//  Created by Kiran Shrestha on 2/1/25.
+//
+
+import SwiftUI
+
+@main
+struct AbsoluteUnitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
